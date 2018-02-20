360a84c0285bc90b0977ee525417037e3b48ba2a
jirawan <tanggwahjirawan@gmail.com>
2018-02-20 12:01:33 +0700

    add Fragment success

--- app/src/main/java/tangkwa/suru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/tangkwa/suru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,26 @@ package tangkwa.suru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import tangkwa.suru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState== null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFrgment,new MainFragment()).commit();
+
+        }
+
+
+
+
+    }  //Main Method
+
+}  // Main class
